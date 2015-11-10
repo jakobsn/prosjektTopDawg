8a94abcd25a94f5f0b21f69be37a1d3a3d0f1d30
Håvard <sirkittelsen@gmail.com>
2015-11-10 20:06:05 +0100

    headSpin

--- script/script.js
+++ script/script.js
@@ -15,3 +15,27 @@ function showMenu(){
     droppedDown = true;
   }
 }
+var looper;
+var degrees = 0;
+function morro(){
+  var elem = document.getElementById("morroknappdiv");
+  if(navigator.userAgent.match("Chrome")){
+		elem.style.WebkitTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("Firefox")){
+		elem.style.MozTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("MSIE")){
+		elem.style.msTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("Opera")){
+		elem.style.OTransform = "rotate("+degrees+"deg)";
+	} else {
+		elem.style.transform = "rotate("+degrees+"deg)";
+	}
+  if(degrees > 359){
+		degrees = 0;
+	}
+  else{
+    looper = setTimeout(morro,1);
+  }
+  degrees++;
+  degrees++;
+}
